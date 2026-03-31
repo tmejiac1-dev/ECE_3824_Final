54f626c1cb8bba2eb040ee275746f8d50ef88ebd
ZhaoXiang Lan <tuv12540@temple.edu>
2026-03-31 12:01:24 -0400

    update the gitignore and secrets.h for esp32

---
+++ Esp32-Door-Usage-Monitor/src/secrets.h
@@ -0,0 +1,19 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+// Wifi configuration
+constexpr const char* ssidList[] = {
+
+};
+
+constexpr const char* passwordList[] = {
+
+};
+
+constexpr int wifiCount = sizeof(ssidList) / sizeof(ssidList[0]);
+
+constexpr const char* serverUrl = "";// Sserver URL
+
+constexpr const char* apiKey = ""; // API key for backend authentication
+
+
+#endif 
