b26134f08405e534bd288c5bf39981bfcfa19675
ZhaoXiang Lan <tuv12540@temple.edu>
2026-04-01 13:39:22 -0400

    correct the folder structure

--- Esp32-Door-Usage-Monitor/src/secrets.h
+++ Esp32-Door-Usage-Monitor/src/secrets.h
@@ -2,18 +2,22 @@
 #define SECRETS_H
 // Wifi configuration
 constexpr const char* ssidList[] = {
-
+  "ssovo(2.4)",
+  "ovo",
+  "ssovo"
 };
 
 constexpr const char* passwordList[] = {
-
+  "LanZhaoXiang0607",
+  "20030607",
+  "20030607"
 };
 
 constexpr int wifiCount = sizeof(ssidList) / sizeof(ssidList[0]);
 
-constexpr const char* serverUrl = "";// Sserver URL
+constexpr const char* serverUrl = "https://project-mqrua.vercel.app/api/data";// Sserver URL
 
-constexpr const char* apiKey = ""; // API key for backend authentication
+constexpr const char* apiKey = "LanZhaoXiang123"; // API key for backend authentication
 
 
 #endif 
